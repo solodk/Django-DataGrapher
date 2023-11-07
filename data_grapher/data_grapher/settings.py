1048dc823b375276b216d512c2552fc2334a65f5
solodk <solodk.m@gmail.com>
2023-11-07 13:08:38 +0200

    hide secrests

--- data_grapher/data_grapher/settings.py
+++ data_grapher/data_grapher/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from . import secrets
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0nwhdt5r$z^11&!1jamx-e%%v6x9lu&%=v023=%zt*3fi9lqzi'
+SECRET_KEY = secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
