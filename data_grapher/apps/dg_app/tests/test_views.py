bdefa445f62639cfbccdd5bf47f7778ce3ff4310
solodk <solodk.m@gmail.com>
2023-11-13 10:45:18 +0200

    DDG-7 Test fix

--- data_grapher/apps/dg_app/tests/test_views.py
+++ data_grapher/apps/dg_app/tests/test_views.py
@@ -1,13 +1,25 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from django.urls import reverse
+from apps.dg_app.models import Project, Table
 
 class ViewTests(TestCase):
     def setUp(self):
         self.client = Client()
+        self.table_data = {'key': 'value'}
+
         self.user = User.objects.create(
             username='test_user', 
             password='testpassword'
+        )
+        self.project = Project.objects.create(
+            name='test_project',
+            owner=self.user
+        )
+        self.table = Table.objects.create(
+            project=self.project,
+            content=self.table_data,
+            name='test_table'
         )
 
     def test_index_view(self):
@@ -21,8 +33,7 @@ class ViewTests(TestCase):
         response = self.client.get(reverse('dg_app:home'))
 
         self.assertEqual(response.status_code, 200)
-        # Check if the content for authenticated users is present
-        self.assertContains(response, '<a>Projects</a>')
+        self.assertContains(response, '<a href="/projects/">Projects</a>')
         self.assertContains(response, '<a>Templates</a>')
         self.assertContains(response, '<p>{Latest Templates}</p>')
 
