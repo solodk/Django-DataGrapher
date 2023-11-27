f562ed1b428dd1b79be11515ca1a57531fe56a02
solodk <solodk.m@gmail.com>
2023-11-27 08:08:46 +0200

    DDG-13 Edit table feature

--- data_grapher/apps/dg_app/urls.py
+++ data_grapher/apps/dg_app/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('projects/', views.projects, name='projects'),
     path('projects/<int:project_id>', views.project, name='project'),
-    path('table/<int:table_id>', views.table, name='table')
+    path('table/<int:table_id>', views.table, name='table'),
+    path('table/<int:table_id>/edit', views.edit_table, name='edit_table'),
+    path('table/<int:table_id>/save', views.save_table, name='save_table'),
 ]
--- data_grapher/dg_project/urls.py
+++ data_grapher/dg_project/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.dg_app.urls')),
     path('accounts/', include('apps.accounts.urls')),
 ]
+
+# Serve static files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
