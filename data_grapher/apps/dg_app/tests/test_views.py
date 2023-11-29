64aa717f2a6995529c5d2e6b3a88dff451185843
solodk <solodk.m@gmail.com>
2023-11-29 04:23:53 +0200

    delete dg_app views test

--- data_grapher/apps/dg_app/tests/test_views.py
+++ data_grapher/apps/dg_app/tests/test_views.py
@@ -21,36 +21,3 @@ class ViewTests(TestCase):
             content=self.table_data,
             name='test_table'
         )
-
-    def test_index_view(self):
-        response = self.client.get(reverse('dg_app:index'))
-
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'dg_app/index.html')
-    
-    def test_logged_homepage_view(self):
-        self.client.force_login(self.user)
-        response = self.client.get(reverse('dg_app:home'))
-
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, '<a href="/projects/">Projects</a>')
-        self.assertContains(response, '<a>Templates</a>')
-        self.assertContains(response, '<p>{Latest Templates}</p>')
-
-    def test_unlogged_homepage_view(self):
-        self.client.logout()
-        response = self.client.get(reverse('dg_app:home'))
-
-        # Check if the response status code 
-        # 200 (OK) or 302 (redirect to login page)
-        self.assertIn(response.status_code, [200, 302])
-
-        # Check if the content for unauthenticated users is present
-        if response.status_code == 302:
-            expected_url = reverse('accounts:login') + '?next=' + reverse('dg_app:home')
-            self.assertRedirects(response, expected_url)
-        else:
-            self.assertContains(
-                response, 
-                '<p>You need to be logged in to see this page</p>'
-            )
